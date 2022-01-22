69338f164d0ce34e27b4d623a57777ce34b80da6
TheDerpLo <49573402+TheDerpLo@users.noreply.github.com>
2022-01-22 12:04:18 -0600

    commiy data types, sentences & fortune teller

---
+++ Basic Data Types Activity/story_time(sean cresswell).js
@@ -0,0 +1,37 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+
+let str1 = 'Bonjour.';
+let str2 = 'Bob';
+let str3 = 'Jerry the Destroyer';
+let str4 = 'the IRS';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+let int1 = 5;
+let int2 = 57;
+let int3 = 7;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+let array_items = ['fine china', 'comically large forks', 'big iron'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+let sellingComicallyLargeUtensils = false;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+
+console.log('Bob: ' + str1 + " Je m'appelle (my name is) " + str2 + '.');
+console.log('I work for ' + str3 + ' and am currently on the run from the ' + str4);
+console.log('for stealing ' + int1 + ' million dollars.');
+console.log('Me: Are you selling comically large utensils?');
+console.log("Bob: Yes I'm selling " + int3 + ' ' + array_items[1] + ' for ' + int2 + ' each.');
+console.log("Also with the purchase of 5 forks I can put in some of my " + array_items[0] + ' for half price.')
+console.log("Me: Ok cool, is that a " + array_items[2] + ' on your hip?')
+console.log("Bob: Indeed that is a " + array_items[2] + ' on my hip.');
